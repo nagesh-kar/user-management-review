75efc8e2ff76460a5ff59d005dc1ab1a3c2366bc
nagesh-kar <nagesh21.ks@gmail.com>
2026-03-04 11:50:48 +0530

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,11 @@
+import { NextFunction, Request, Response } from "express";
+
+export const authenticate = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): void => {
+  // Assume this securely authenticates a request.
+  console.log("Request authenticated");
+  next();
+};
